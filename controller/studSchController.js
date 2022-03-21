5ac7a48dc25832146f825ab40c147321bc421aad
AravindMende259 <aravindmende@gmail.com>
2022-03-21 21:50:11 +0530

    Update,delete,getById

--- controller/studSchController.js
+++ controller/studSchController.js
@@ -29,6 +29,53 @@ export const getStudents = async (request, response) => {
       }
   }
    
+
+  export const getStudentById = async (request, response) => {
+    try{
+        const stud = await Student.findById(request.params.id);
+        response.status(200).json(stud);
+    }catch( error ){
+        response.status(404).json({ message: error.message })
+    }
+}
+
+// Save data of edited user in the database
+export const editStudent = async (request, response) => {
+  //   let stud = await Student.findById(request.params.id);
+  //  // stud = request.body;
+  //   console.log(stud,"usdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdf")
+  //   const editStudent = new Student(stud);
+  //   try{
+  //       await Student.findOneAndUpdate({ _id: mongoose.Types.ObjectId(req.params.id) }, editStudent);
+        
+  //       response.status(201).json(editStudent);
+  //   } catch (error){
+  //       response.status(409).json({ message: error.message});     
+  //   }
+
+  let stud;
+  [err, stud] = await to(
+    Student.findOneAndUpdate(
+      { _id: mongoose.Types.ObjectId(request.params.id) },
+      request.body
+    )
+  );
+  if (err) {
+    return response.status(500).json({ Error: err });
+  }
+  return response.status(200).json(stud);
+
+}
+
+// deleting data of user from the database
+export const deleteStudent = async (request, response) => {
+    try{
+        await Student.deleteOne({_id: request.params.id});
+        response.status(201).json("User deleted Successfully");
+    } catch (error){
+        response.status(409).json({ message: error.message});     
+    }
+}
   
 
 
